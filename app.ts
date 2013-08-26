1cb4e884a50c975f6195e63d5e0ae06c99197d25
Dave Compton <to.dave.c@gmail.com>
2013-08-26 12:11:11 -0700

    first pass at entering two numbers, left padding with zeros and displaying them.

--- app.ts
+++ app.ts
@@ -1,28 +1,30 @@
-class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+/// <reference path="angularjs/angular.d.ts" />
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
-
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
+function padLeft(nr:string, n:number, str?:string) {
+    var padding = Array(n - nr.length + 1).join(str || '0');
+    return padding+nr;
 }
 
-window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
-};
+class multiplier {
+    constructor($scope) {
+        $scope.m0 = "";
+        $scope.m1 = "";
+
+        $scope.digitize0 = (m0: string, m1: string): string[] => {
+            var maxlen = Math.max(m0.length, m1.length);
+            if (maxlen == 0) {
+                return ['0'];
+            }
+
+            var plen = 1;
+            while (plen < maxlen)
+            {
+                plen *= 2;
+            }
+            plen *= 2;
+
+            var padded = padLeft(m0, plen);
+            return padded.split('');
+        }
+    }
+}
