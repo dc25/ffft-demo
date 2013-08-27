493f46770745220e277427c3435550d31c88f353
Dave Compton <to.dave.c@gmail.com>
2013-08-27 11:29:14 -0700

    converting to complex numbers and displaying in coefficient array.

--- app.ts
+++ app.ts
@@ -1,68 +1,82 @@
 /// <reference path="angularjs/angular.d.ts" />
 
 class Complex {
-    constructor(public re: number, public im: number) {
-    }
+    constructor(public re: number, public im: number) {}
 
     static add(c0:Complex, c1:Complex) : Complex {
         var res = new Complex(c0.re + c1.re, c0.im + c1.im);
         return res;
     }
-
     
     static mul(c0: Complex, c1: Complex) : Complex {
         var res = new Complex(c0.re*c1.re - c0.im * c1.im, c0.re*c1.im + c0.im*c1.re);
         return res;
     }
 
+    public toString() {
+        return "" + this.re + "+"  + this.im + "i";
+    }
+}
+
+class Transform {
+
+    constructor(public primaryValue: string, public secondaryValue: string) {
+        this.compute();
+    }
+
+    public coefficients: Complex[][];
+
+    compute() : void {
+        var maxlen = Math.max(this.primaryValue.length, this.secondaryValue.length);
+
+        var columnCount = 1;
+        var rowCount = 0;
+        while (columnCount < maxlen)
+        {
+            columnCount *= 2;
+            rowCount += 1;
+        }
+        columnCount *= 2;
+        rowCount += 1;
+
+        this.coefficients = [];
+
+        var workingRow = [];
+        for (var c = 0; c < columnCount; c++) {
+            var digit = (c < this.primaryValue.length) ? this.primaryValue[this.primaryValue.length-c-1] : 0;
+            workingRow.push(new Complex(digit, 0));
+        }
+        this.coefficients.push(workingRow);
+
+        for (var r = 1; r < rowCount; r++) {
+            workingRow = [];
+            for (var c = 0; c < columnCount; c++) {
+                workingRow.push(new Complex(0, 0));
+            }
+            this.coefficients.push(workingRow);
+        }
+    }
+
+    updatePrimary(newValue:string) {
+
+    }
 }
 
 class multiplier {
     constructor($scope) {
-        $scope.m0 = "";
-        $scope.m1 = "";
+        $scope.m0 = "4567";
+        $scope.m1 = "3256";
 
-        $scope.transformations = [];
-
-        $scope.transformationRows = (m0: string, m1: string): number[] => {
-            var maxlen = Math.max(m0.length, m1.length);
-
-            var plen = 1;
-            var rowCount = 0;
-            while (plen < maxlen)
-            {
-                plen *= 2;
-                rowCount += 1;
-            }
-            plen *= 2;
-            rowCount += 1;
-
-            var res = [];
-            for (var i = 0; i < rowCount; i++) {
-                res.push(i);
-            }
-            return res;
-        }
-
-        $scope.transformationColumns = (m0: string, m1: string): number[]=> {
-            var maxlen = Math.max(m0.length, m1.length);
-
-            var columnCount = 1;
-            var rowCount = 0;
-            while (columnCount < maxlen)
-            {
-                columnCount *= 2;
-                rowCount += 1;
-            }
-            columnCount *= 2;
-            rowCount += 1;
-
-            var res = [];
-            for (var i = 0; i < columnCount; i++) {
-                res.push(i);
-            }
-            return res;
-        }
-
+        $scope.t0 = new Transform($scope.m0, $scope.m1);
+        $scope.t1 = new Transform($scope.m1, $scope.m0);
+        /*
+        $scope.$watch("m0",
+            function (newValue, oldValue) {
+                if (newValue != oldValue) {
+                    t0.updatePrimary(m0);
+                    t1.updateSecondary(m0);
+                }
+            });
+        */
     }
 }
