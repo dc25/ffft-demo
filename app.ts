f39084c75e6ed201299ff5365563d50066e56c3c
Dave Compton <to.dave.c@gmail.com>
2013-08-27 08:09:17 -0700

    added a Complex class

--- app.ts
+++ app.ts
@@ -1,26 +1,28 @@
 /// <reference path="angularjs/angular.d.ts" />
 
+class Complex {
+    constructor(public re: number, public im: number) {
+    }
+
+    static add(c0:Complex, c1:Complex) : Complex {
+        var res = new Complex(c0.re + c1.re, c0.im + c1.im);
+        return res;
+    }
+
+    
+    static mul(c0: Complex, c1: Complex) : Complex {
+        var res = new Complex(c0.re*c1.re - c0.im * c1.im, c0.re*c1.im + c0.im*c1.re);
+        return res;
+    }
+
+}
+
 class multiplier {
     constructor($scope) {
         $scope.m0 = "";
         $scope.m1 = "";
 
-        $scope.digitize0 = (m0: string, m1: string): string[] => {
-            var maxlen = Math.max(m0.length, m1.length);
-            if (maxlen == 0) {
-                return ['0'];
-            }
-
-            var plen = 1;
-            while (plen < maxlen)
-            {
-                plen *= 2;
-            }
-            plen *= 2;
-
-            var padded = Array(plen - m0.length + 1).join('0') + m0;
-            return padded.split('');
-        }
+        $scope.transformations = [];
 
         $scope.transformationRows = (m0: string, m1: string): number[] => {
             var maxlen = Math.max(m0.length, m1.length);
