ca6aa19eab103d77bfe270dc44ec78a0696211ba
Dave Compton <to.dave.c@gmail.com>
2013-08-26 17:52:24 -0700

    now printing out a table of data for the first input number - nothing interesting in the table yet.

--- app.ts
+++ app.ts
@@ -21,5 +21,46 @@ class multiplier {
             var padded = Array(plen - m0.length + 1).join('0') + m0;
             return padded.split('');
         }
+
+        $scope.transformationRows = (m0: string, m1: string): number[] => {
+            var maxlen = Math.max(m0.length, m1.length);
+
+            var plen = 1;
+            var rowCount = 0;
+            while (plen < maxlen)
+            {
+                plen *= 2;
+                rowCount += 1;
+            }
+            plen *= 2;
+            rowCount += 1;
+
+            var res = [];
+            for (var i = 0; i < rowCount; i++) {
+                res.push(i);
+            }
+            return res;
+        }
+
+        $scope.transformationColumns = (m0: string, m1: string): number[]=> {
+            var maxlen = Math.max(m0.length, m1.length);
+
+            var columnCount = 1;
+            var rowCount = 0;
+            while (columnCount < maxlen)
+            {
+                columnCount *= 2;
+                rowCount += 1;
+            }
+            columnCount *= 2;
+            rowCount += 1;
+
+            var res = [];
+            for (var i = 0; i < columnCount; i++) {
+                res.push(i);
+            }
+            return res;
+        }
+
     }
 }
