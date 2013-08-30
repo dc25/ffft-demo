d99d192981e7fffbac9379703b4ccf71cc3bec7d
Dave Compton <to.dave.c@gmail.com>
2013-08-30 16:59:12 -0700

    got multiplication working.

--- app.ts
+++ app.ts
@@ -1,5 +1,11 @@
 /// <reference path="angularjs/angular.d.ts" />
 
+
+function formattedNumber(n:number):string {
+    var r = 100;
+    return "" + Math.round(r * n) / r;
+}
+
 class Complex {
     constructor(public re: number, public im: number) {}
 
@@ -19,7 +25,7 @@ class Complex {
 
     public toString() {
         var r = 100;
-        var res = "" + Math.round(r * this.re) / r;
+        var res = formattedNumber(this.re);
         if (this.im == 0.0) {
             return res;
         }
@@ -30,107 +36,141 @@ class Complex {
             res += "-";
             imPart = -imPart;
         }
-        res += (Math.round(r * imPart) / r + "i");
+        res += (formattedNumber(imPart) + "i");
         return res;
     }
 }
 
 class Transform {
 
-    constructor(public primaryValue: string, public secondaryValue: string) {
-        this.computeTransform();
-    }
-
     public coefficients: Complex[][];
 
-    computeCoefficient(row: number, column: number, bitCount: number): Complex {
+    computeCoefficient(row: number, column: number, columnCount: number, forward: boolean): Complex {
         var t = column >>> row;
         var s = column - (t << row);
         var bitMask = (1 << (row - 1)) - 1; // to mask all but the high bit of s.
         var sHighBitRemoved = s & bitMask;
         var iRe = (t << (row - 1)) + sHighBitRemoved;
-        var iIm = iRe + (1 << (bitCount - 1));
-        var rotation = Complex.powerOfE(2 * Math.PI * column / (1 << row));
+        var iIm = iRe + columnCount / 2;
+        var exp = 2 * Math.PI * column / (1 << row);
+        if (!forward) {
+            exp *= -1;
+        }
+        var rotation = Complex.powerOfE(exp);
         return Complex.add(this.coefficients[row - 1][iRe], Complex.mul(rotation, this.coefficients[row - 1][iIm]));
     }
 
-    computeCoefficients(firstRow: Complex[], rowCount: number): void {
+    constructor(firstRow: Complex[], forward?:boolean) {
 
-        var bitCount = rowCount - 1;
+        if (forward === undefined) {
+            forward = true;
+        }
+
         var columnCount = firstRow.length;
-        
+        var rowCount = 0;
+        for (var cc = columnCount; cc > 0; cc >>>= 1) {
+            rowCount++;
+        }
+
         this.coefficients = [];
         this.coefficients.push(firstRow);
  
         for (var r = 1; r < rowCount; r++) {
             var workingRow = [];
             for (var c = 0; c < columnCount; c++) {
-                var coef = this.computeCoefficient(r, c, bitCount);
+                var coef = this.computeCoefficient(r, c, columnCount, forward);
                 workingRow.push(coef);
             }
             this.coefficients.push(workingRow);
         }
     }
-    
-    computeTransform() : void {
-        var maxlen = Math.max(this.primaryValue.length, this.secondaryValue.length);
 
-        var columnCount = 1;
-        var bitCount = 0;
-        while (columnCount < maxlen)
+    lastRow(): Complex[]{
+        return this.coefficients[this.coefficients.length - 1];
+    }
+
+    scaledLastRow(): number[] {
+        var lr = this.lastRow();
+        var res:number[] = [];
+        for (var i = 0; i < lr.length; i++) {
+            res.push(lr[i].re/lr.length);
+        }
+        return res;
+    }
+
+    digitsLastRow(): number[]{
+        var slr = this.scaledLastRow();
+        var res: number[] = [];
+        var carry0 = 0;
+        for (var i = 0; i < slr.length; i++)
         {
-            columnCount *= 2;
-            bitCount += 1;
+            var totalDigit = slr[i] + carry0+.001; // .001 to assure tiny negative numbers don't get floored to -1
+            var carry1 = Math.floor((totalDigit) / 10);
+            res.push(totalDigit - carry1 * 10);
+            carry0 = carry1;
         }
-        columnCount *= 2;
-        bitCount += 1;
-        var rowCount = bitCount + 1;
-
-
-        var workingRow = [];
-        for (var c = 0; c < columnCount; c++) {
-            var digit:number = (c < this.primaryValue.length) ? parseInt(this.primaryValue[this.primaryValue.length-c-1]) : 0;
-            workingRow.push(new Complex(digit, 0));
-        }
-
-        this.computeCoefficients(workingRow, rowCount);
+        return res;
     }
-
-    updatePrimary(newValue: string):void {
-        this.primaryValue = newValue;
-        this.computeTransform();
-    }
-
-    updateSecondary(newValue: string): void {
-        this.secondaryValue = newValue;
-        this.computeTransform();
-    }
-
 }
 
 class multiplier {
+
+    integersToCoefficients(primaryValue: string, secondaryValue: string): Complex[] {
+
+        var maxlen = Math.max(primaryValue.length, secondaryValue.length);
+
+        var columnCount = 1;
+        for (maxlen--; maxlen; maxlen>>>=1) {
+            columnCount<<=1;
+        }
+        columnCount <<= 1;
+
+        var res:Complex[] = [];
+        for (var c = 0; c < columnCount; c++) {
+            var digit: number = (c < primaryValue.length) ? parseInt(primaryValue[primaryValue.length - c - 1]) : 0;
+            res.push(new Complex(digit, 0));
+        }
+
+        return res;
+
+    }
+
+    convolute(r0: Complex[], r1: Complex[]): Complex[] {
+        var res:Complex[] = [];
+        for (var i = 0; i < r1.length; i++) {
+            res[i] = Complex.mul(r0[i], r1[i]);
+        }
+        return res;
+    }
+
+
+    compute($scope: any):void {
+        $scope.t0 = new Transform(this.integersToCoefficients($scope.m0, $scope.m1));
+        $scope.t1 = new Transform(this.integersToCoefficients($scope.m1, $scope.m0));
+        $scope.product = new Transform(this.convolute($scope.t0.lastRow(), $scope.t1.lastRow()), false);
+        $scope.scaled = $scope.product.scaledLastRow();
+        $scope.result = $scope.product.digitsLastRow();
+    }
+
+
     constructor($scope) {
         $scope.m0 = "2642";
         $scope.m1 = "5821";
 
-        $scope.t0 = new Transform($scope.m0, $scope.m1);
-        $scope.t1 = new Transform($scope.m1, $scope.m0);
+        this.compute($scope);
 
         $scope.$watch("m0",
-            function (newValue:string, oldValue:string):void {
+            (newValue:string, oldValue:string):void => {
                 if (newValue != oldValue) {
-                    $scope.t0.updatePrimary(newValue);
-                    $scope.t1.updateSecondary(newValue);
+                    this.compute($scope);
                 }
             });
 
         $scope.$watch("m1",
-            function (newValue: string, oldValue: string): void {
+            (newValue: string, oldValue: string): void => {
                 if (newValue != oldValue) {
-                    $scope.t1.updatePrimary(newValue);
-                    $scope.t0.updateSecondary(newValue);
+                    this.compute($scope);
                 }
             });
-
     }
 }
